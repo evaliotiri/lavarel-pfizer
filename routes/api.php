e7ef62d12647ba46dda9d847049ae3b217a1422d
eliotiri <evaliotiri@gmail.com>
2020-11-30 16:26:57 +0200

    Added new methods for route DepartmentsManagers.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\DepartmentsController;
+use App\Http\Controllers\API\DepartmentsManagersController;
 use App\Http\Controllers\API\DepartmentsUsersController;
 use App\Http\Controllers\API\UsersVacationsController;
 use Illuminate\Http\Request;
@@ -25,9 +26,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/users', UsersController::class);
-Route::apiResource('/skills', SkillsController::class);
+Route::apiResource('/skills', SkillsController::class)->middleware(\App\Http\Middleware\Logger::class);
 Route::apiResource('/departments', DepartmentsController::class);
-Route::apiResource('/users/{user}/skills', UsersSkillsController::class);
-Route::apiResource('/users/{user}/vacations', UsersVacationsController::class);
+Route::apiResource('users.skills', UsersSkillsController::class);
+Route::apiResource('users.vacations', UsersVacationsController::class);
 Route::apiResource('departments.users', DepartmentsUsersController::class);
-Route::post('/departments/{department}/assignManager/{user}', [DepartmentsController::class, 'assignManager']);
+Route::apiResource('departments.managers', DepartmentsManagersController::class);
