f31aaa47ab300ec6981d02bb35caf41571c9f288
eliotiri <evaliotiri@gmail.com>
2020-11-30 15:18:51 +0200

    Added foreign key manager_id for table User

--- database/migrations/2019_9_9_095925_create_departments_table.php
+++ database/migrations/2019_9_9_095925_create_departments_table.php
@@ -18,6 +18,7 @@ class CreateDepartmentsTable extends Migration
             $table->timestamps();
             $table->string('title');
 
+            $table->unsignedBigInteger('manager_id')->nullable();
         });
     }
 
